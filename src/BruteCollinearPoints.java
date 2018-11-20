5b38b3a5aaf8aea070bbc0405b03147fa8b2f506
Black Hawk <taghikhani.sh@gmail.com>
2018-11-19 22:03:05 -0500

    brute force part

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points){
+         double m = points[0].slopeTo(points[1]);
+         double n = points[0].slopeTo(points[2]);
+         double o = points[0].slopeTo(points[3]);
+
+         if (m == n && m == o){
+                LineSegment q = new LineSegment(points[0], points[1]);
+         }
+         return q;
+    }  // finds all line segments containing 4 points
+    public           int numberOfSegments()        // the number of line segments
+    public LineSegment[] segments()                // the line segments
 }
