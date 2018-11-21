f043a62a9c15c2a7778060987d8b447b03a7b032
Black Hawk <taghikhani.sh@gmail.com>
2018-11-21 09:05:13 -0500

    tetsing process

---
+++ src/test.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] arg) throws FileNotFoundException {
+        File file = new File(arg[0]);
+
+        Scanner in = new Scanner(file);
+
+        int n = in.nextInt();
+        Point[] points = new Point[n];
+
+        for (int i = 0; )
+
+
+
+    }
+
+
+}
