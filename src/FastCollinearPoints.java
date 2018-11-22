53317837121965fac336ba92ab72b1ad6301deef
Black Hawk <taghikhani.sh@gmail.com>
2018-11-21 20:52:35 -0500

    assignment 3 passed

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,4 +1,3 @@
-import org.omg.CORBA.TRANSACTION_UNAVAILABLE;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -8,11 +7,8 @@ public class FastCollinearPoints {
     private LinkedList<LineSegment> segment = new LinkedList<>();
     private int numOfSegments = 0;
     private LineSegment[] segments;
- //   private LineSegment tempsegment;
-//    private LinkedList<LineSegmentPoint> SegmentPoint;
-//    private LinkedList<Integer> EndLinPosition;
     private boolean duplicate;
-    private int EndPoint;
+
 
 
     public FastCollinearPoints(Point[] points) {
@@ -23,10 +19,11 @@ public class FastCollinearPoints {
         } catch (NullPointerException e) {
             throw new IllegalArgumentException();
         }
-        double[][] slopes = new double[points.length][2];
+
         for (int i = 0; i < points.length; i++) {
-            for (int j = i + 1; j < points.length; j++) {
-                if (points[i].equals(points[j]) || points[i] == null || points[j] == null) {
+            double[][] slopes = new double[points.length][2];
+            for (int j = 0; j < points.length; j++) {
+                if (( i!=j&& points[i].equals(points[j])) || points[i] == null || points[j] == null) {
                     throw new IllegalArgumentException();
                 }
                 slopes[j][0] = (double) j;
@@ -41,34 +38,21 @@ public class FastCollinearPoints {
 
             //Arrays.sort(slopes);
 
-            EndPoint = i;
+
             for (int t = 0; t < points.length - 2; t++) {
                 if (slopes[t][1] == slopes[t + 1][1]
                         && slopes[t][1] == slopes[t+1][1]
                         && slopes[t][1] == slopes[t+2][1]) {
-                    if((int)slopes[t][0] > EndPoint){
-                        EndPoint = (int)slopes[t][0];
-                    }
-                    if((int)slopes[t+1][0] > EndPoint){
-                        EndPoint = (int)slopes[t][0];
-                    }
-                    if((int)slopes[t+2][0] > EndPoint){
-                        EndPoint = (int)slopes[t][0];
-                    }
 
                     t = t + 2;
                     while (t < points.length - 1&&
                             slopes[t][1] == slopes[t+1][1]){
-                        if((int)slopes[t+1][0] > EndPoint){
-                            EndPoint = (int)slopes[t+1][0];
-                        }
-
                         t++;
                     }
 
                     for(int m = 0; m < i; m++){
-
-                        if(points[m].slopeTo(points[EndPoint]) == points[i].slopeTo(points[EndPoint])) {
+                        if(points[m].slopeTo(points[(int )slopes[t][0]]) ==
+                                points[i].slopeTo(points[(int)slopes[t][0]])) {
                             duplicate = true;
                         }
                     }
@@ -76,6 +60,8 @@ public class FastCollinearPoints {
                         segment.add(new LineSegment(points[i], points[(int) slopes[t][0]]));
                         numOfSegments++;
                     }
+
+                    duplicate = false;
 
 
                 }
@@ -94,19 +80,4 @@ public class FastCollinearPoints {
     public LineSegment[] segments() {
         return segments;
     }
-//
-//    private class LineSegmentPoint {
-//        private final Point s;   // one endpoint of this line segment
-//        private final Point f;   // the other endpoint of this line segment
-//
-//
-//        private LineSegmentPoint(Point s, Point f) {
-//            if (s == null || f == null) {
-//                throw new NullPointerException("argument is null");
-//            }
-//            this.s = s;
-//            this.f = f;
-//        }
-//
-//    }
 }
